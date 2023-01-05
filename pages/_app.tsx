c3f5209850e5ea50a11893b794079bf256668349
Quessr <Quessr00@gmail.com>
2023-01-05 19:58:56 +0900

    :package: styled-components 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { counterSlice } from "../store/features/counter";
+import styled from "styled-components";
 
 export default function Counter() {
   const dispatch = useAppDispatch();
@@ -8,6 +9,7 @@ export default function Counter() {
   });
   return (
     <>
+      <Title>Hello World!</Title>
       <button
         onClick={() => {
           dispatch(counterSlice.actions.increment());
@@ -29,3 +31,9 @@ export default function Counter() {
     </>
   );
 }
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
