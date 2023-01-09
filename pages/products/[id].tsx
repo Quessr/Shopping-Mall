8499176cb47424a2753cbe45ad2c1611a94d1ea2
Quessr <Quessr00@gmail.com>
2023-01-09 22:16:08 +0900

    ✨ ProductDetailPage 컴포넌트 구현

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,11 +1,27 @@
 import { useQuery } from "react-query";
-import { QueryKeys } from "../../utils/fetcher";
+import { Product } from "../../components/product/types";
+import { fetcher, QueryKeys } from "../../utils/fetcher";
+import { useRouter } from "next/router";
+import ProductDetail from "../../components/product/detail";
 
-const ProductDetail = () => {
-//   const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
-//   fetcher({ method: "GET", path: "/products" })
-// );
-  return <div>상세페이지</div>;
+const ProductDetailPage = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
+    fetcher({ method: "GET", path: `/products/${id}` })
+  );
+
+  console.log('id',id)
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <h2>상품상세</h2>
+      <ProductDetail item={data} />
+    </div>
+  );
 };
 
-export default ProductDetail;
+export default ProductDetailPage;
